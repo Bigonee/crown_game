262ebf5fc196acc13604188a2d9c33f5e5f8acc0
oriolikart <oriol.icart@appandgosolutions.com>
2020-03-20 15:17:50 +0100

    no message

---
+++ Source/MUG_Corona/TileInterface.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TileInterface.h"
+
+// Add default functionality here for any ITileInterface functions that are not pure virtual.
