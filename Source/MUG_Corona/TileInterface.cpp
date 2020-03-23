feef2e48fdb60e154749710da6862af913f8c44f
oriolikart <oriol.icart@appandgosolutions.com>
2020-03-23 18:08:33 +0100

    [DEV] Remove TileInterface

--- Source/MUG_Corona/TileInterface.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "TileInterface.h"
-
-// Add default functionality here for any ITileInterface functions that are not pure virtual.
